93e566e544b9e5a3030ed028c494ce43cecf5342
Dario Grau <dgrau@linets.cl>
2024-02-28 17:25:00 -0300

    Refactor DataProvider model in QuickProductPositioning
    Removed unused dependencies like PoolInterface and DataPersistorInterface from the DataProvider model of the QuickProductPositioning module. Additionally, simplified the getData method by removing unnecessary logic and streamlining the handling of item data.

--- app/code/Omnipro/QuickProductPositioning/Model/DataProvider.php
+++ app/code/Omnipro/QuickProductPositioning/Model/DataProvider.php
@@ -4,63 +4,43 @@ declare(strict_types=1);
 namespace Omnipro\QuickProductPositioning\Model;
 
 use Magento\Ui\DataProvider\AbstractDataProvider;
-use Magento\Ui\DataProvider\Modifier\ModifierInterface;
-use Magento\Ui\DataProvider\Modifier\PoolInterface;
-use Magento\Framework\App\Request\DataPersistorInterface;
 use Omnipro\QuickProductPositioning\Model\ResourceModel\FeaturedProductModel\FeaturedProductCollectionFactory;
 
 class DataProvider extends AbstractDataProvider
 {
-    private PoolInterface $pool;
-
-    protected $loadedData;
+    protected ?array $loadedData = null;
     protected $collection;
 
-    protected $dataPersistor;
-
     public function __construct(
-        FeaturedProductCollectionFactory $featuredProductCollectionFactory,
-        PoolInterface $pool,
         $name,
         $primaryFieldName,
         $requestFieldName,
-        DataPersistorInterface $dataPersistor,
+        FeaturedProductCollectionFactory $featuredProductCollectionFactory,
         array $meta = [],
         array $data = []
     ) {
-        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
         $this->collection = $featuredProductCollectionFactory->create();
-        $this->dataPersistor = $dataPersistor;
-        $this->pool = $pool;
-        $this->meta = $this->prepareMeta($this->meta);
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
     }
 
-    public function prepareMeta(array $meta): array
+    /**
+     * Get data
+     *
+     * @return array The data for the object.
+     */
+    public function getData(): array
     {
-        $meta = parent::getMeta();
+        if (null === $this->loadedData) {
+            $this->loadedData = [];
 
-        /** @var ModifierInterface $modifier */
-        foreach ($this->pool->getModifiersInstances() as $modifier) {
-            $meta = $modifier->modifyMeta($meta);
-        }
-        return $meta;
-    }
+            $items = $this->collection->getItems();
 
-    public function getData()
-    {
-        if (isset($this->loadedData)) {
-            return $this->loadedData;
-        }
-        $items = $this->collection->getItems();
-        foreach ($items as $model) {
-            $this->loadedData[$model->getId()] = $model->getData();
-        }
-        $data = $this->dataPersistor->get('omnipro_featured_products_model');
-        if (!empty($data)) {
-            $model = $this->collection->getNewEmptyItem();
-            $model->setData($data);
-            $this->loadedData[$model->getId()] = $model->getData();
-            $this->dataPersistor->clear('omnipro_featured_products_model');
+            foreach ($items as $item) {
+                $itemData = $item->getData();
+                $itemData['products'] = $itemData['product_id'];
+                $itemData['categories'] = explode(',', $itemData['categories']);
+                $this->loadedData[$item->getEntityId()] = $itemData;
+            }
         }
 
         return $this->loadedData;
